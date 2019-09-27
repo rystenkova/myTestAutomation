f9d62f08418eceacc63c02a59687de9f8c02234f
halyna.rystenkova <halyna.rystenkova@globallogic.com>
2019-09-27 13:42:23 +0300

    my code before 1111

--- src/main/java/PayModeTests.java
+++ src/main/java/PayModeTests.java
@@ -1,7 +1,11 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -29,5 +33,26 @@ public class PayModeTests {
     @Test
     public void test() {
         driver.get("https://pmx-qa3-cs.saas-n.com/px/login");
+        WebElement username = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@name='username']")));
+        username.sendKeys("IOKTIABROV_QA3_I@paymode.com");
+        WebElement password = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@name='password']")));
+        password.sendKeys("Test1QAi234");
+        WebElement submit = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@type='submit']")));
+        submit.click();
+        wait.until(ExpectedConditions.titleContains("Verify Your Account - Paymode-X"));
+        WebElement radiobutton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@value='OOBSMS_primary']")));
+        radiobutton.click();
+        WebElement contin = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@type='submit']")));
+        contin.click();
+        wait.until(ExpectedConditions.titleContains("Verify Your Account"));
+
+
+        String value = username.getText();
+
+        Assert.assertEquals("string", "string", "Strings must be equal");
+
+
+        driver.findElement(By.name("username"));
+//new Select(login).selectByValue("value");
     }
 }
